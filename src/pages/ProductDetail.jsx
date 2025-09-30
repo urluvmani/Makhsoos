5899e4d0915e9619087a675acb7f1b5c1aae0558
Hafiz Muhammad Abdul Rehman <abdulrehman90190@gmail.com>
2025-09-30 11:11:50 -0700

    Updated frontend changes

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -5,20 +5,20 @@ import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/slices/cartSlice"; // ✅ local cart action
 import { FiArrowLeft } from "react-icons/fi"; // ✅ back icon
 
-
 const ProductDetail = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const [product, setProduct] = useState(null);
   const [qty, setQty] = useState(1); // ✅ user can select quantity
 
-    const Navigate = useNavigate(); // ✅ navigation hook
-
+  const Navigate = useNavigate(); // ✅ navigation hook
+        const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5173";
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
         const { data } = await API.get(`/products/${id}`);
+
         setProduct(data);
       } catch (error) {
         console.error("❌ Error fetching product:", error);
@@ -45,23 +45,29 @@ const ProductDetail = () => {
 
   return (
     <main className="bg-[#1a1a1a] text-white min-h-screen flex flex-col md:items-center  justify-center md:gap-20 py-5 px-[5%]">
-         <button
-                onClick={() => Navigate("/")}
-                className="flex items-center md:absolute md:top-10 md:left-10 gap-2 text-[#a67c52] mb-6 hover:text-[#8a6745] transition"
-              >
-                <FiArrowLeft className="text-xl" />
-                <span>Back to Home</span>
-              </button>
+      <button
+        onClick={() => Navigate("/")}
+        className="flex items-center md:absolute md:top-10 md:left-10 gap-2 text-[#a67c52] mb-6 hover:text-[#8a6745] transition"
+      >
+        <FiArrowLeft className="text-xl" />
+        <span>Back to Home</span>
+      </button>
       <div className="flex flex-col md:flex-row items-center gap-12 max-w-4xl">
-        <img
-          src={product.image}
-          alt={product.name}
-          className="w-80 h-96 object-cover rounded-lg shadow-lg"
-        />
+
+<img
+  src={`${baseURL}/${product.image}`}
+  alt={product.name}
+  className="w-80 h-96 object-cover rounded-lg shadow-lg"
+/>
+
         <div>
-          <h1 className="text-3xl sm:text-4xl font-light mb-4">{product.name}</h1>
+          <h1 className="text-3xl sm:text-4xl font-light mb-4">
+            {product.name}
+          </h1>
           <p className="text-lg opacity-80 mb-6">{product.description}</p>
-          <div className="text-2xl text-[#a67c52] mb-6">Rs. {product.price}</div>
+          <div className="text-2xl text-[#a67c52] mb-6">
+            Rs. {product.price}
+          </div>
 
           {/* Quantity Controls */}
           <div className="flex items-center gap-3 mb-6">
